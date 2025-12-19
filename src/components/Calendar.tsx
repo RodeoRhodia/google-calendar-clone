3249eb9f9bc9ce5640055ef23d64709dd8453e05
Lanz <lanzdagamer@gmail.com>
2025-12-19 13:52:06 -0500

    instantiate useLocalStorage and EventContext

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,14 +1,19 @@
 import { CalendarHeader } from "./CalendarHeader";
 import { CalendarGrid } from "./CalendarGrid";
 import { useState } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import { EventContext, type EventsMap } from "../contexts/EventContext";
 
 export default function Calendar() {
-	const [currentDate, setCurrentDate] = useState<Date>(new Date());
+    const [currentDate, setCurrentDate] = useState<Date>(new Date());
+    const [events, setEvents] = useLocalStorage<EventsMap>("EVENTS", {});
 
     return (
-        <div className="h-full flex flex-col text-gray-800">
-            <CalendarHeader currentDate={currentDate} setCurrentDate={setCurrentDate}/>
-            <CalendarGrid currentDate={currentDate}/>
-        </div>
+        <EventContext.Provider value={{ events, setEvents }}>
+            <div className="h-full flex flex-col text-gray-800">
+                <CalendarHeader currentDate={currentDate} setCurrentDate={setCurrentDate}/>
+                <CalendarGrid currentDate={currentDate}/>
+            </div>
+        </EventContext.Provider>
     );
 }
