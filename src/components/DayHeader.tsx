7782dfe5e6d3f34ed528ffbde004ae06286d673f
Lanz <lanzdagamer@gmail.com>
2025-12-20 17:46:20 -0500

    remove ability to add events by clicking header only

--- src/components/DayHeader.tsx
+++ src/components/DayHeader.tsx
@@ -1,49 +1,28 @@
-import { useState } from "react";
-import { format } from "date-fns";
-import { AddEventForm } from "./AddEventForm";
-
 interface DayHeaderProps {
     weekName?: string;
     dayNumber: number;
     isToday?: boolean;
-    date: Date;
 }
 
 export function DayHeader({
     weekName,
     dayNumber,
     isToday = false,
-    date,
 }: DayHeaderProps) {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
     return (
-        <>
-            <div className="mb-1 flex flex-col items-center relative">
-                {weekName && (
-                    <div className="uppercase text-xs font-bold text-gray-500">
-                        {weekName}
-                    </div>
-                )}
-                <div
-                    className={`text-sm w-6 h-6 flex justify-center items-center ${
-                        isToday ? "bg-blue-calendar rounded-full text-white" : ""
-                    }`}
-                >
-                    {dayNumber}
+        <div className="mb-1 flex flex-col items-center">
+            {weekName && (
+                <div className="uppercase text-xs font-bold text-gray-500">
+                    {weekName}
                 </div>
-                <button
-                    className="opacity-0 absolute bg-none border-none rounded-full w-6 h-6 flex justify-center items-center right-0 top-0 text-xl cursor-pointer text-gray-800 hover:bg-gray-100 group-hover:opacity-100 focus:opacity-100"
-                    onClick={() => setIsModalOpen(true)}
-                >
-                    +
-                </button>
+            )}
+            <div
+                className={`text-sm w-6 h-6 flex justify-center items-center ${
+                    isToday ? "bg-blue-calendar rounded-full text-white" : ""
+                }`}
+            >
+                {dayNumber}
             </div>
-            <AddEventForm
-                isOpen={isModalOpen}
-                onClose={() => setIsModalOpen(false)}
-                date={format(date, "M/d/yy")}
-            />
-        </>
+        </div>
     );
 }
